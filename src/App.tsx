3b6d003d626c201146ea6fe74ce91a5518f82cfb
Yasin Walum <ywalum@gmail.com>
2025-03-15 14:34:52 +0300

    starter template

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import ModeToggle from "./components/mode-toggle";
-import TextAnalyzer from "./components/text-analyzer";
 
 export default function App() {
   return (
-    <>
-      <ModeToggle />
-      <TextAnalyzer />
-    </>
+    <main className="min-h-dvh grid place-items-center">
+      <div className="flex flex-col gap-3 items-center justify-center">
+        <ModeToggle />
+        <h1 className="text-7xl font-bold text-center">Starter</h1>
+      </div>
+    </main>
   );
 }
