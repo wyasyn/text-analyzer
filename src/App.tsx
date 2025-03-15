746c7879b943aabe58fdec6af2e66b0d5dcac277
Yasin Walum <ywalum@gmail.com>
2025-03-15 04:29:32 +0300

    starter

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import ModeToggle from "./components/mode-toggle";
+import TextAnalyzer from "./components/text-analyzer";
+
+export default function App() {
+  return (
+    <>
+      <ModeToggle />
+      <TextAnalyzer />
+    </>
+  );
+}
---
+++ src/components/mode-toggle.tsx
@@ -0,0 +1,31 @@
+import { Toggle } from "@/components/ui/toggle";
+import { useTheme } from "@/context/theme-provider";
+import { MoonIcon, SunIcon } from "lucide-react";
+
+export default function ModeToggle() {
+  const { theme, setTheme } = useTheme();
+
+  return (
+    <div>
+      <Toggle
+        variant="outline"
+        className="group dark:hover:bg-muted size-9 dark:bg-transparent"
+        pressed={theme === "dark"}
+        onPressedChange={(pressed) => setTheme(pressed ? "dark" : "light")}
+        aria-label={`Switch to ${theme === "dark" ? "light" : "dark"} mode`}
+      >
+        {/* Note: After dark mode implementation, rely on dark: prefix rather than group-data-[state=on]: */}
+        <MoonIcon
+          size={16}
+          className="shrink-0 scale-0 opacity-0 transition-all dark:scale-100 dark:opacity-100"
+          aria-hidden="true"
+        />
+        <SunIcon
+          size={16}
+          className="absolute shrink-0 scale-100 opacity-100 transition-all dark:scale-0 dark:opacity-0"
+          aria-hidden="true"
+        />
+      </Toggle>
+    </div>
+  );
+}
